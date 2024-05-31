a301e585614013aaf61a1347122a0a84cf42741f
Piero Valentino Noa Chahuayo <piero.noa@tecsup.edu.pe>
2024-05-31 11:47:46 -0500

    fin del primer caso propuesssto

---
+++ src/app/users/user-list/user-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit{
+  users = [
+    { id : 1, name: 'Jhon'},
+    { id : 2, name: 'Jane'},
+    { id : 3, name: 'Doe'}
+  ];
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+}
